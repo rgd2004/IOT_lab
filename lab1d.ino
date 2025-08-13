c6a0361a851ed463ab15be68e681e7f820ffec69
rgd2004 <153278291+rgd2004@users.noreply.github.com>
2025-08-13 15:46:18 +0530

    Add files via upload

---
+++ lab1d.ino
@@ -0,0 +1,15 @@
+void setup(){
+  pinMode(2,OUTPUT);
+  pinMode(9,INPUT);
+}
+void loop(){
+  int status=0;
+  status=digitalRead(9);
+  if(status==1){
+    digitalWrite(2,HIGH);
+    delay(200);
+  }
+  else{
+    digitalWrite(2,LOW);
+    delay(200);
+  }}
